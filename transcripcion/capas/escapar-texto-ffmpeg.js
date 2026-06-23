86796531ca5f6bec3bbb5d83cb10c0f07cc757b2
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-22 23:34:14 -0500

    Crear escapador de texto FFmpeg

---
+++ transcripcion/capas/escapar-texto-ffmpeg.js
@@ -0,0 +1,14 @@
+export function escaparTextoFfmpeg(texto) {
+  return String(texto || '')
+    .replace(/\\/g, '\\\\')
+    .replace(/'/g, "\\\\'")
+    .replace(/:/g, '\\:')
+    .replace(/\[/g, '\\[')
+    .replace(/\]/g, '\\]')
+    .replace(/,/g, '\\,')
+    .replace(/\r?\n/g, ' ')
+    .replace(/\s+/g, ' ')
+    .trim();
+}
+
+export default escaparTextoFfmpeg;
