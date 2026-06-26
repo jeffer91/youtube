10ab17a6f36bb2de888312da4b59057b4f11e686
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 17:26:39 -0500

    Bloque 17: agregar verificacion agrupada

---
+++ scripts/verificar-bloque-17-autovideo.js
@@ -0,0 +1,5 @@
+/* Verificacion agrupada Bloque 17 AutoVideoJeff. */
+
+import './verificar-produccion-reemplazo-ui.js';
+
+console.log('OK Bloque 17 AutoVideoJeff: reemplazo y aprendizaje en Produccion conectados.');
