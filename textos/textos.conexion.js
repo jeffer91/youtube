4d85aef98aa58210ef5a293011a05d29545208e6
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 12:06:11 -0500

    Bloque 2: conectar validacion de textos

--- textos/textos.conexion.js
+++ textos/textos.conexion.js
@@ -9,3 +9,4 @@ export { generarTextosPantalla } from './generar-textos-pantalla.service.js';
 export { generarGraficosVisuales } from './generar-graficos.service.js';
 export { generarTablaVisual, generarTablasVisuales } from './generar-tablas-visuales.service.js';
 export { calcularPosicionTexto, aplicarPosicionesTextos } from './posicionar-textos.service.js';
+export { validarTextoPantalla, validarTextosPantalla } from './validar-textos.service.js';
