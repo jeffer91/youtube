e7c3bf9f600ed4c33f19787341e9e0823845fbfe
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 18:38:31 -0500

    Bloque 21: agregar verificacion agrupada

---
+++ scripts/verificar-bloque-21-autovideo.js
@@ -0,0 +1,5 @@
+/* Verificacion agrupada Bloque 21 AutoVideoJeff. */
+
+import './verificar-auditoria-integral-autovideo.js';
+
+console.log('OK Bloque 21 AutoVideoJeff: auditoria integral conectada.');
