47c5f04ef767eb65234c3ef421511beea980ee74
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 17:16:40 -0500

    Bloque 15: agregar verificacion agrupada

---
+++ scripts/verificar-bloque-15-autovideo.js
@@ -0,0 +1,5 @@
+/* Verificacion agrupada Bloque 15 AutoVideoJeff. */
+
+import './verificar-produccion-acciones-ui.js';
+
+console.log('OK Bloque 15 AutoVideoJeff: acciones UI de Produccion conectadas.');
