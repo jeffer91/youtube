d416237aa91d5087fe84c7f784c8baf906cb5668
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 11:44:21 -0500

    Bloque 1: agregar conexion de exportacion

---
+++ exportacion/exportacion.conexion.js
@@ -0,0 +1,12 @@
+/*
+  Modulo: exportacion
+  Funcion: punto unico de conexion para exportaciones por plataforma.
+*/
+
+export { PLATAFORMAS_EXPORTACION, PLATAFORMAS_DEFECTO, obtenerIdsPlataformas, obtenerPlataformaExportacion } from './plataformas.config.js';
+export { crearExportacionModelo, normalizarPlataformas, validarExportacionModelo } from './exportacion.modelo.js';
+export { prepararExportaciones } from './preparar-exportaciones.service.js';
+export { crearPlanExportacionVertical } from './exportar-vertical.service.js';
+export { crearPlanExportacionHorizontal } from './exportar-horizontal.service.js';
+export { crearPlanExportacionCuadrado } from './exportar-cuadrado.service.js';
+export { validarExportacion, validarExportaciones } from './validar-exportacion.service.js';
