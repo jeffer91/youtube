e5440f4d714b81671b1a80c2cd9a5ab97fbeaf66
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 12:06:24 -0500

    Bloque 2: conectar plan avanzado de audio

--- audio/audio.conexion.js
+++ audio/audio.conexion.js
@@ -111,3 +111,10 @@ export async function mejorarAudioVideo({ entrada, entendimiento, opciones = {} 
     );
   }
 }
+
+export { detectarRuidoInicial } from './detectar-ruido-inicial.service.js';
+export { crearPlanLimpiezaAudio, resumirPlanLimpiezaAudio } from './limpiar-audio.service.js';
+export { crearPlanSilenciarSegmento, crearPlanesSilencio } from './silenciar-segmento.service.js';
+export { crearPlanCorteInicioRuidoso } from './cortar-inicio-ruidoso.service.js';
+export { crearPlanAudio } from './audio-plan.service.js';
+export { diagnosticarPlanAudio } from './diagnostico-audio.service.js';
