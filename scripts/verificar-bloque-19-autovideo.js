146318551ffe070c2193e66acadcdcb44fba865a
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 17:41:56 -0500

    Bloque 19: agregar verificacion agrupada

---
+++ scripts/verificar-bloque-19-autovideo.js
@@ -0,0 +1,5 @@
+/* Verificacion agrupada Bloque 19 AutoVideoJeff. */
+
+import './verificar-diagnostico-fuerte.js';
+
+console.log('OK Bloque 19 AutoVideoJeff: diagnostico fuerte y reintento conectados.');
