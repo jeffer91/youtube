ca033067fae799015e9c01bdddd4af16c353a15c
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 12:14:47 -0500

    Bloque 4: agregar conexion de biblioteca

---
+++ biblioteca/biblioteca.conexion.js
@@ -0,0 +1,13 @@
+/*
+  Modulo: biblioteca
+  Funcion: punto unico de conexion para biblioteca general.
+*/
+
+export { BIBLIOTECA_CONFIG, obtenerTiposBiblioteca } from './biblioteca.config.js';
+export { CATEGORIAS_BIBLIOTECA, obtenerCategoriaBiblioteca, listarCategoriasBiblioteca } from './categorias.config.js';
+export { crearRecursoModelo, validarRecursoModelo, limpiarNombreRecurso } from './recurso.modelo.js';
+export { listarRecursosBiblioteca, obtenerRutaIndiceBiblioteca } from './listar-recursos.service.js';
+export { guardarRecursoBiblioteca } from './guardar-recurso.service.js';
+export { buscarRecursosBiblioteca, buscarPrimerRecurso } from './buscar-recurso.service.js';
+export { clasificarRecurso } from './clasificar-recurso.service.js';
+export { validarLicenciaRecurso, marcarLicenciaRevisada } from './validar-licencia-recurso.service.js';
