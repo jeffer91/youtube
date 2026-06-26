69f9b10e2193b88e0b701dbf9a032cfcac60f91b
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 12:05:44 -0500

    Bloque 2: agregar conexion de textos

---
+++ textos/textos.conexion.js
@@ -0,0 +1,11 @@
+/*
+  Modulo: textos
+  Funcion: punto unico de conexion para textos, graficos y tablas.
+*/
+
+export { TEXTOS_CONFIG, obtenerLimiteTextosPorPerfil } from './textos.config.js';
+export { detectarTextosRelevantes } from './detectar-textos-relevantes.service.js';
+export { generarTextosPantalla } from './generar-textos-pantalla.service.js';
+export { generarGraficosVisuales } from './generar-graficos.service.js';
+export { generarTablaVisual, generarTablasVisuales } from './generar-tablas-visuales.service.js';
+export { calcularPosicionTexto, aplicarPosicionesTextos } from './posicionar-textos.service.js';
