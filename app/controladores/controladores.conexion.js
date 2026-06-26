4fc0552af53e60bdb5e43af56e8835a7c215f7c3
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 12:33:19 -0500

    Bloque 7: agregar conexion de controladores

---
+++ app/controladores/controladores.conexion.js
@@ -0,0 +1,7 @@
+export { crearNuevoProyectoController } from './nuevo-proyecto.controller.js';
+export { crearProcesadoController } from './procesado.controller.js';
+export { crearProduccionController } from './produccion.controller.js';
+export { crearResultadoController } from './resultado.controller.js';
+export { crearBibliotecaController } from './biblioteca.controller.js';
+export { crearPerfilesController } from './perfiles.controller.js';
+export { crearDiagnosticoController } from './diagnostico.controller.js';
