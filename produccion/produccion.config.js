79e1f8f768456eba8cdd96330d8b412b03c8fc4a
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-27 08:07:42 -0500

    Bloque 5 ampliar configuracion produccion timeline

--- produccion/produccion.config.js
+++ produccion/produccion.config.js
@@ -4,14 +4,16 @@
 */
 
 export const PRODUCCION_CONFIG = Object.freeze({
-  version: '1.0.0',
+  version: '1.1.0',
   archivoPlan: 'plan-produccion.json',
   estados: Object.freeze({
     borrador: 'borrador',
     enRevision: 'en_revision',
     aprobado: 'aprobado',
     requiereCambios: 'requiere_cambios',
-    listoExportar: 'listo_exportar'
+    listoExportar: 'listo_exportar',
+    noUsar: 'no_usar',
+    reemplazado: 'reemplazado'
   }),
   tiposElemento: Object.freeze({
     recurso: 'recurso',
@@ -22,7 +24,18 @@ export const PRODUCCION_CONFIG = Object.freeze({
     fondo: 'fondo',
     zoom: 'zoom',
     efecto: 'efecto',
+    animacion: 'animacion',
+    imagen: 'imagen',
     audio: 'audio'
+  }),
+  pistasLineaTiempo: Object.freeze({
+    subtitulos: 'subtitulos',
+    textos: 'textos-y-titulos',
+    imagenes: 'imagenes-y-recursos',
+    animaciones: 'animaciones',
+    efectos: 'efectos-visuales',
+    audio: 'audio',
+    otros: 'otros'
   }),
   modos: Object.freeze({
     automaticoRapido: 'automatico_rapido',
