a9eab608f32ff03461d754913434bf6ecb64d1d7
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-27 00:04:43 -0500

    Bloque 14 exportar presets visuales

---
+++ editar/efectos/presets/index.js
@@ -0,0 +1,2 @@
+export { PRESETS_VISUALES_EFECTOS, obtenerPresetVisualEfectos, listarPresetsVisualesEfectos } from './presets-visuales.config.js';
+export { aplicarPresetVisualAContexto, aplicarPresetVisualASeleccion } from './aplicar-preset-visual.service.js';
