65c02080de23d621e0a4f555e9d93c1e47e4ef79
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-27 00:00:28 -0500

    Bloque 13 exportar aprendizaje de efectos

---
+++ editar/efectos/aprendizaje/index.js
@@ -0,0 +1,3 @@
+export { obtenerRutaMemoriaEfectos, cargarMemoriaEfectos, guardarMemoriaEfectos, obtenerRegistroPerfil, obtenerRegistroEfecto, normalizarMemoriaEfectos } from './memoria-efectos.service.js';
+export { aplicarAprendizajeEfectos } from './aplicar-aprendizaje-efectos.service.js';
+export { registrarAprendizajeEfectos } from './registrar-aprendizaje-efectos.service.js';
