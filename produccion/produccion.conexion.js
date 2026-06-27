059d26ee48482e0e6cfb25ff76b5ab897ad82433
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-27 08:08:22 -0500

    Bloque 5 exportar timeline produccion

--- produccion/produccion.conexion.js
+++ produccion/produccion.conexion.js
@@ -1,10 +1,11 @@
 /*
   Modulo: produccion
-  Funcion: punto unico de conexion para revision y aprobacion.
+  Funcion: punto unico de conexion para revision, linea de tiempo y aprobacion.
 */
 
 export { PRODUCCION_CONFIG } from './produccion.config.js';
 export { crearPlanProduccionModelo, crearElementoProduccion, validarPlanProduccion } from './produccion.modelo.js';
+export { crearLineaTiempoProduccion, sincronizarLineaTiempoConElementos, obtenerPistaProduccion } from './linea-tiempo-produccion.service.js';
 export { crearPlanProduccion } from './crear-plan-produccion.service.js';
 export { guardarPlanProduccion, cargarPlanProduccion, obtenerRutaPlanProduccion } from './cargar-plan-produccion.service.js';
 export { reemplazarRecursoProduccion } from './reemplazar-recurso-produccion.service.js';
