e475a9899518a51781ea73e8e4e1f35205144c78
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 22:44:33 -0500

    Bloque 3 exportar planificador de efectos

---
+++ editar/efectos/planificador/index.js
@@ -0,0 +1,3 @@
+export { validarPlanEfectos } from './validar-plan-efectos.service.js';
+export { seleccionarEfectosLocal } from './seleccionar-efectos-local.service.js';
+export { planificarEfectos } from './planificar-efectos.service.js';
