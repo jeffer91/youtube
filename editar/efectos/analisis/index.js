e14d2b94c680d9623c12f799f8702c7805e0e106
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 22:41:52 -0500

    Bloque 2 exportar analisis de efectos

---
+++ editar/efectos/analisis/index.js
@@ -0,0 +1,3 @@
+export { normalizarMomentoTimeline, mapearMomentosATimeline } from './mapear-momentos-a-timeline.service.js';
+export { detectarMomentosEfecto } from './detectar-momentos-efecto.service.js';
+export { analizarContextoVideoEfectos } from './contexto-video-efectos.service.js';
