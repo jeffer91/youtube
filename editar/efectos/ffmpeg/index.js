401af2962bec1e91f487e86cca2f9e00ee70b62e
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-26 22:53:10 -0500

    Bloque 4 exportar compilador ffmpeg de efectos

---
+++ editar/efectos/ffmpeg/index.js
@@ -0,0 +1,4 @@
+export { TIPOS_RECETA_FFMPEG, RECETAS_FFMPEG_EFECTOS, obtenerRecetaFfmpeg, tieneRecetaFfmpeg } from './efectos-ffmpeg.recetas.js';
+export { compilarEfectoFfmpeg } from './compilar-efecto-ffmpeg.service.js';
+export { validarFiltroFinalFfmpeg } from './validar-filtro-final.service.js';
+export { compilarPlanFfmpeg } from './compilar-plan-ffmpeg.service.js';
