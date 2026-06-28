6bdd0fc7bedee09148895222231a8596a225d32a
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-28 12:20:23 -0500

    Exportar servicios base de biblioteca simple

--- biblioteca/biblioteca.conexion.js
+++ biblioteca/biblioteca.conexion.js
@@ -1,12 +1,38 @@
 /*
   Modulo: biblioteca
-  Funcion: punto unico de conexion para biblioteca general.
+  Funcion: punto unico de conexion para biblioteca general permanente.
 */
 
-export { BIBLIOTECA_CONFIG, obtenerTiposBiblioteca } from './biblioteca.config.js';
-export { CATEGORIAS_BIBLIOTECA, obtenerCategoriaBiblioteca, listarCategoriasBiblioteca } from './categorias.config.js';
-export { crearRecursoModelo, validarRecursoModelo, limpiarNombreRecurso } from './recurso.modelo.js';
-export { listarRecursosBiblioteca, obtenerRutaIndiceBiblioteca } from './listar-recursos.service.js';
+export {
+  BIBLIOTECA_CONFIG,
+  ALCANCES_BIBLIOTECA,
+  ESTADOS_TECNICOS_RECURSO,
+  TIPOS_ARCHIVO_BIBLIOTECA,
+  FORMATOS_RECURSO_BIBLIOTECA,
+  obtenerTiposBiblioteca,
+  obtenerFormatosBiblioteca,
+  obtenerExtensionesPermitidasBiblioteca,
+  extensionPermitidaBiblioteca
+} from './biblioteca.config.js';
+export { ESTILOS_VIDEO_BASE, listarEstilosVideo, obtenerEstiloVideo, normalizarEstiloVideo, normalizarListaEstilosVideo } from './estilos-video.config.js';
+export { CATEGORIAS_BIBLIOTECA, obtenerCategoriaBiblioteca, listarCategoriasBiblioteca, normalizarCategoriaBiblioteca, crearCategoriaEditable } from './categorias.config.js';
+export {
+  obtenerRutaBibliotecaRaiz,
+  obtenerRutasBibliotecaGeneral,
+  asegurarEstructuraBibliotecaGeneral,
+  obtenerCarpetaDestinoRecursoGeneral,
+  crearNombreArchivoBiblioteca,
+  crearRutaWebBiblioteca,
+  existeRutaArchivo
+} from './rutas-biblioteca.service.js';
+export {
+  crearRecursoModelo,
+  validarRecursoModelo,
+  limpiarNombreRecurso,
+  detectarTipoArchivoBiblioteca,
+  detectarFormatoInicialRecurso
+} from './recurso.modelo.js';
+export { listarRecursosBiblioteca, obtenerRutaIndiceBiblioteca, leerIndiceBibliotecaGeneral, escribirIndiceBibliotecaGeneral } from './listar-recursos.service.js';
 export { guardarRecursoBiblioteca } from './guardar-recurso.service.js';
 export { buscarRecursosBiblioteca, buscarPrimerRecurso } from './buscar-recurso.service.js';
 export { clasificarRecurso } from './clasificar-recurso.service.js';
