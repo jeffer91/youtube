b16d325f7d541347dba5d22a4b7afdcac098b3cd
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-06-29 07:06:13 -0500

    Conectar wizard visual entendimiento

--- app/navegacion/navegacion-bootstrap.js
+++ app/navegacion/navegacion-bootstrap.js
@@ -3,6 +3,7 @@ import { inicializarDiagnosticoFuerteUI } from '../diagnostico-fuerte-ui.js';
 import { inicializarAuditoriaIntegralUI } from '../auditoria-integral-ui.js';
 import { inicializarResultadoFinalUI } from '../resultado-final-ui.js';
 import { inicializarEntendimientoUI } from '../etapas-ui/entendimiento-ui.js';
+import { inicializarEntendimientoWizardUI } from '../etapas-ui/entendimiento-wizard-ui.js';
 import { inicializarBibliotecaProyectoUI } from '../biblioteca-proyecto-ui.js';
 import { inicializarPlanEdicionUI } from '../etapas-ui/plan-edicion-ui.js';
 import { inicializarProduccionMaestroUI } from '../etapas-ui/produccion-maestro-ui.js';
@@ -38,6 +39,7 @@ function iniciarNavegacion() {
   inicializarAuditoriaIntegralUI();
   inicializarResultadoFinalUI();
   inicializarEntendimientoUI();
+  inicializarEntendimientoWizardUI();
   inicializarBibliotecaProyectoUI();
   inicializarPlanEdicionUI();
   inicializarProduccionMaestroUI();
